ead7b36b1e2e4b2c2bea9705727729c44723aa74
Chris <mdhatter215@hotmail.com>
2016-09-20 11:20:11 -0700

    Add files via upload

---
+++ lunch_money2.py
@@ -0,0 +1,23 @@
+#lunch_money2.py
+#http://www.ardendertat.com/2011/09/17/programming-interview-questions-1-array-pair-sum/
+
+#Find pairs or people who have $1.00 for lunch and match them up
+#given a list, find matches (pairs) that add up to 100; reject ones that don't have a match or odd numbers
+
+def pairSum2(arr, k):
+    if len(arr)<2:
+        return
+    seen=set()
+    output=set()
+    for num in arr:
+        target=k-num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add( (min(num, target), max(num, target)) )
+            
+    print '\n'.join( map(str, list(output)) )
+
+
+myarray=[50,50,60,40,90,20,10,80,13,12,10,87,97,3,50]
+pairSum2(myarray, k=100)
