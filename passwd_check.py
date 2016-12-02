89cd282eeafaa61cccb7f730166f3cb539659a30
Chris <mdhatter215@hotmail.com>
2016-12-01 16:52:21 -0800

    Update passwd_check.py
    way better method of performing these checks, can't believe I didn't know about the 're' class (shaking my head!)

--- passwd_check.py
+++ passwd_check.py
@@ -1,7 +1,7 @@
 ##https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises.txt
 ##Question 18
 ##Level 3
-
+##
 ##Question:
 ##A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
 ##Following are the criteria for checking the password:
@@ -19,7 +19,13 @@
 ##ABd1234@1
 ##                              
 ##
+# Sample input:
+#ABd1234@1,a F1#,2w3E*,2We3345,sdfGGK$33
 
+
+##################################################
+## Debug Class for Verbose Messages
+##################################################
 ##Python 2.7
 ##Requires enum-0.4.6 ("pip install enum")
 from enum import Enum
@@ -32,46 +38,43 @@ class VERBOSITY(Enum):
     NONE=   0
 
 
-myVerbo=VERBOSITY.NONE
+myVerbo=VERBOSITY.MEDIUM
 print 'VERBOSITY = %s' %myVerbo
+##################################################
+import re
+value = []
+items=[x for x in raw_input('> ').split(',')]
 
-####################################
-#Check for upper case character, lower case character, or digit
-def check_chars(myinput, casecheck=''):
-    if (myVerbo>=VERBOSITY.HIGH): print 'inside body of check_chars -> %s  casecheck -> %s' %(myinput, casecheck)
-    if casecheck=='upper': myrange=(ord('a'), ord('z'))     #ASCII DEC 97-122
-    elif casecheck=='lower': myrange=(ord('A'), ord('Z'))   #ASCII DEC 65-90
-    elif casecheck=='digit': myrange=(ord('0'), ord('9'))   #ASCII DEC 48-57
-    else:
-        print 'unsupported parameter to check_chars function'; raise SystemExit
-    if (myVerbo>=VERBOSITY.HIGH): print myrange
-    x,y=myrange
-    for i, j in ((i, j) for i in range(len(myinput)) for j in range(x,y+1)):
-        if (myVerbo>=VERBOSITY.DEBUG): print 'i = %d  j = %d' %(i,j)
-        if (myVerbo>=VERBOSITY.DEBUG): print 'myinput[i] = %s   chr(j) = %s' %(myinput[i], chr(j))
-        if myinput[i] == chr(j): return True
-        else: continue
-    return False
+if (myVerbo.value>=VERBOSITY.HIGH.value): print items
 
-####################################
-#Check for a special character $#@
-def check_special_char(myinput):
-    if (myVerbo>=VERBOSITY.HIGH): print 'inside body of check_special_char -> %s' %myinput
-    if '$' in myinput or '#' in myinput or '@' in myinput: return True
-    else: return False
+for p in items:
+  myflag = 1
+  ###############################
+  #upper check
+  if not re.search("[A-Z]", p):
+    myflag = 0
+  ###############################
+  #digit check
+  elif not re.search("[0-9]", p):
+    myflag = 0
+  ###############################
+  #lower check
+  elif not re.search("[a-z]", p):
+    myflag = 0
+  ###############################
+  #special char check
+  elif not re.search("[$#@]", p):
+    myflag = 0
+  ###############################
+  #length check
+  elif not (6 <= len(p) <= 12):
+    myflag = 0
+  ##debug statement
+  if (myVerbo.value>=VERBOSITY.HIGH.value): print myflag
+  ###############################
+  # if passed all checks, add to the list
+  if (myflag):
+    value.append(p)
+  
 
-####################################
-#Check passwd length at least 6 but no more than 12
-def check_length(myinput):
-    if (myVerbo>=VERBOSITY.HIGH): print 'inside body of check_length -> %s' %myinput
-    if (6 <= len(myinput) <= 12): return True
-    else: return False
-
-
-###########################
-# Start of program
-###########################
-USER_INPUT = raw_input('> ')
-myinput=USER_INPUT.split(',')
-for passwd in myinput:
-    if check_chars(passwd, 'upper') and check_chars(passwd, 'lower') and check_chars(passwd, 'digit') and check_special_char(passwd) and check_length(passwd): print passwd
+print ",".join(value)
